b32e61074e89716cc1e792104f399220d5ce2038
DUPIN <maximedupin1992@gmail.com>
2024-04-21 17:28:42 +0200

    fix

---
+++ src/api/artit/controllers/artit.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * artit controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::artit.artit');
---
+++ src/api/artit/services/artit.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * artit service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::artit.artit');
